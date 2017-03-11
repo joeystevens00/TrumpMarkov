f56f793ddf2d403e62d8dc51f02ad1dd5ed4a27e
jstevens00 <joeystevens00@gmail.com>
2017-03-11 13:12:42 -0500

    refactor

--- export_tweets.rb
+++ export_tweets.rb
@@ -1,58 +1,57 @@
 # Exports all tweets from a user into a plaintext file
 require 'dotenv/load'
 require 'twitter'
-class ExportTweets
+
+class TweetExporter
   # Exchange your oauth_token and oauth_token_secret for an AccessToken instance.
-  @tweets = ''
-  def prepare_access_token(oauth_token, oauth_token_secret, access_token, access_secret)
-    client = Twitter::REST::Client.new do |config|
+  def initialize(oauth_token, oauth_token_secret, access_token, access_secret, outfile="exported_tweets-#{Time.now.to_i}.out.txt")
+    @tweets = []
+    @outfile=outfile
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = oauth_token
       config.consumer_secret     = oauth_token_secret
-      config.access_token        =  access_token
+      config.access_token        = access_token
       config.access_token_secret = access_secret
     end
-    client
   end
-  def get_tweets(client, user)
-    @tweets=client.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=#{user}&count=200&include_rts=false")
+
+  def get_tweets(user)
+    @tweets = @client.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=#{user}&count=200&include_rts=false")
     self
   end
-  def get_tweets_with_max_id(client, user, maxId)
-    @tweets=client.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=#{user}&max_id=#{maxId}&count=200&include_rts=false")
+
+  def get_tweets_with_max_id(user, maxId)
+    @tweets = @client.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=#{user}&max_id=#{maxId}&count=200&include_rts=false")
     self
   end
-  def add_tweet_to_file(tweet, outfile='exported_tweets.out.txt')
-    open(outfile, 'a') do |f|
-      f.puts tweet
-    end
-    self
-  end
-  def add_tweets_to_file(offset)
-    @tweets.drop(offset).each do |tweet|
-      add_tweet_to_file(tweet[:text])
-      self
+
+  def append_to_file(skipFirst=false)
+    open(@outfile, 'a') do |f|
+      @tweets.drop(skipFirst ? 1 : 0).each do |tweet|
+        f.puts tweet[:text]
+      end
     end
   end
+
   def get_last_id
     @tweets.last[:id]
   end
-
 end
 
 consumer_key=ENV['CONSUMER_KEY']
-consume_secret=ENV['CONSUMER_SECRET']
+consumer_secret=ENV['CONSUMER_SECRET']
 access_token=ENV['ACCESS_TOKEN']
 access_token_secret=ENV['ACCESS_TOKEN_SECRET']
 
 
 twitter_user='realDonaldTrump'
-client = ExportTweets.new.prepare_access_token(consumer_key, consume_secret, access_token, access_token_secret)
-first_donald_tweets=ExportTweets.new.get_tweets(client, twitter_user)
-id=first_donald_tweets.get_last_id
-first_donald_tweets.add_tweets_to_file(0)
+exporter = TweetExporter.new consumer_key, consumer_secret, access_token, access_token_secret
+exporter.get_tweets(twitter_user)
+last_id = exporter.get_last_id
+exporter.append_to_file
 
 17.times do  # Unfortunately we only can get 3600 tweets (200 tweets at a time)
-  y = ExportTweets.new.get_tweets_with_max_id(client, twitter_user, id)
-  y.add_tweets_to_file(1)
-  id=y.get_last_id
+  exporter.get_tweets_with_max_id(twitter_user, last_id)
+  exporter.append_to_file(true)
+  last_id = exporter.get_last_id
 end
